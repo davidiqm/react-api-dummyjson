335e0d3555ac059f52b6c2d9b0964b3f51b5b594
David Quimé <davidiqm@gmail.com>
2023-10-14 07:49:32 -0600

    detalles

--- src/screens/EditScreen.jsx
+++ src/screens/EditScreen.jsx
@@ -71,7 +71,7 @@ function EditScreen() {
             {/* <input type="hidden" defaultValue={product.id} /> */}
             <div className="mb-6">
               <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-90">Título</label>
-              <input defaultValue={product.title} onChange={(e) => product.title = e.target.value} type="text" id="title" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+              <input defaultValue={product.title} onChange={(e) => product.title = e.target.value} type="text" id="title" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"  required />
             </div>
             <div className="mb-6">
               <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900">Descripción</label>
@@ -98,7 +98,7 @@ function EditScreen() {
             <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
           </form>
           {
-            success ? <div>Exitoso</div> : <div></div>
+            success ? <div className="mt-5">Exitoso</div> : <div></div>
           }
         </div>
       </div>
